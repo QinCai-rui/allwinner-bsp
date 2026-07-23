8ceb9013de25cda881c4df801e3497e95516be02
Raymont Qin <git@qincai.xyz>
2026-07-23 20:50:02 +1200

    fix: 7.1 api changes:

--- drivers/clk/sunxi-ng/ccu_common.c
+++ drivers/clk/sunxi-ng/ccu_common.c
@@ -203,7 +203,7 @@ static struct ccu_reg_dump *ccu_alloc_reg_dump(struct ccu_common **rdump,
 	return rd;
 }
 
-static int ccu_suspend(void)
+static int ccu_suspend(void *data)
 {
 	struct sunxi_clock_reg_cache *reg_cache;
 
@@ -213,7 +213,7 @@ static int ccu_suspend(void)
 	return 0;
 }
 
-static void ccu_resume(void)
+static void ccu_resume(void *data)
 {
 	struct sunxi_clock_reg_cache *reg_cache;
 
@@ -228,6 +228,10 @@ static void ccu_resume(void)
 static struct syscore_ops sunxi_clk_syscore_ops = {
 	.suspend = ccu_suspend,
 	.resume = ccu_resume,
+};
+
+static struct syscore sunxi_clk_syscore = {
+	.ops = &sunxi_clk_syscore_ops,
 };
 #endif
 
@@ -251,7 +255,7 @@ void sunxi_ccu_sleep_init(void __iomem *reg_base,
 
 	mutex_lock(&ccu_reg_cache_lock);
 	if (list_empty(&ccu_reg_cache_list))
-		register_syscore_ops(&sunxi_clk_syscore_ops);
+		register_syscore(&sunxi_clk_syscore);
 
 	reg_cache->reg_base = reg_base;
 	reg_cache->rd_num = nr_rdump;
